36b8a15d0c096615f97ba6664b40921cc5244f7e
C'est-bon <kor.trickster@gmail.com>
2014-05-29 19:19:24 +0900

    (trickster)
    [install db module]
    vertx install io.vertx~mod-mysql-postgresql~0.3.0-SNAPSHOT
    
    [install lang.scala]
    vi [vert.x root]/conf/langs.properites
    scala=io.vertx~lang-scala~1.0.0:org.vertx.scala.platform.impl.ScalaVerticleFactory
    .scala=scala

---
+++ mods/io.vertx~mod-shopcrawler-informSaver~0.0.1-alpha1/informSaver.java
@@ -0,0 +1,99 @@
+import org.vertx.java.core.Handler;
+import org.vertx.java.platform.Verticle;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+
+public class informSaver extends Verticle {
+	static EventBus eb;
+
+	public void start() {
+		eb = vertx.eventBus();
+
+		// db config
+		JsonObject dbConfig = container.config();
+		dbConfig.putString("address", "shop.saver.db");
+		dbConfig.putString("connection","PostgreSQL");
+		dbConfig.putString("host","localhost");
+		dbConfig.putNumber("port", 5432);
+		dbConfig.putString("username", "postgres");
+		dbConfig.putString("password" ,"zmfhffj");
+		dbConfig.putString("database", "shopcrawler");
+
+		/**
+		 * product saver
+		 */
+		Handler<Message> productSaver = new Handler<Message>() {
+			public void handle(Message message) {
+				// insert product
+				if ("insert".equals(message.body())) {
+					System.out.println("saved product");
+
+					//EventBus eb = vertx.eventBus();
+					//eb.publish("shop.saver.complete.product", "");
+				} else if("select".equals(message.body())) {
+					vertx.eventBus().send("shop.saver.query", "select count(*) from product;", new Handler<Message<String>>() {
+						@Override
+						public void handle(Message<String> event) {
+							System.out.println("Result of query:" + event.body());
+						}
+					});
+				}
+			}
+		};
+
+		/**
+		 * mall saver
+		 */
+		Handler<Message> mallSaver = new Handler<Message>() {
+			public void handle(Message message) {
+				// insert product
+				if ("insert".equals(message.body())) {
+					System.out.println("saved mall");
+
+					//EventBus eb = vertx.eventBus();
+					//eb.publish("shop.saver.complete.product", "");
+				}
+			}
+		};
+
+		vertx.eventBus().registerHandler("shop.saver.query", new Handler<Message<String>>() {
+			@Override
+			public void handle(final Message<String> msg) {
+				System.out.println("Received message:" + msg.body());
+
+				// query object
+				JsonObject json = new JsonObject();
+				json.putString("action","raw");
+				json.putString("command", msg.body());
+
+				vertx.eventBus().send("shop.saver.db", json, new Handler<Message<JsonObject>>() {
+					@Override
+					public void handle(Message<JsonObject> dbMsg) {
+						System.out.println("asdg");
+						JsonObject result = dbMsg.body();
+						//System.out.println(result.toString());
+						msg.reply(result.getArray("results").<JsonArray>get(0).get(0).toString());
+					}
+				});
+			}
+		});
+
+		// module
+		container.deployModule ("io.vertx~mod-mysql-postgresql~0.3.0-SNAPSHOT", dbConfig, new Handler<AsyncResult<String>>() {
+			@Override
+			public void handle(AsyncResult<String> event) {
+				System.out.println("Postgres module deployed:" + event.toString() + ",failed:" + event.failed() + ": " + event.cause());
+			}
+		});
+
+		// regist handler
+		eb.registerHandler("shop.saver.product", productSaver);
+		eb.registerHandler("shop.saver.mall", mallSaver);
+
+		// test
+		eb.publish("shop.test.saver", "test");
+	}
+}
--- shopcrawler_launcher.java
+++ shopcrawler_launcher.java
@@ -2,58 +2,51 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.net.NetSocket;
 import org.vertx.java.platform.Verticle;
 import org.vertx.java.core.buffer.Buffer;
-
 import org.vertx.java.core.http.HttpClient;
 import org.vertx.java.core.http.HttpClientRequest;
 import org.vertx.java.core.http.HttpClientResponse;
-
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
-
 import org.vertx.java.core.json.JsonObject;
 
-import org.vertx.java.core.buffer.Buffer;
-
 public class shopcrawler_launcher extends Verticle {
+	static EventBus eb;
 
-  public void start() {
-	EventBus eb = vertx.eventBus();
-	JsonObject config = container.config();
-	
-	// test parser
-	Handler<Message> myHandler = new Handler<Message>() {
+	public void start() {
+		eb = vertx.eventBus();
+		JsonObject config = container.config();
 
-		public void handle( Message message ){
-			System.out.println("I received a message " + message.body() );
+		// test parser
+		/*Handler<Message> myHandler = new Handler<Message>() {
+			public void handle( Message message ){
+				System.out.println("I received a message " + message.body() );
 
-			EventBus eb = vertx.eventBus();
-			eb.publish("shop.download.parse", "http://192.168.0.53/");
-		}
-
-	};
-	Handler<Message> shop_load_complete = new Handler<Message>() {
-
-		public void handle( Message message ){
-			System.out.println("I received a message " + message.body() );
-
-			// initialized downloader ( test mode )
-			if( "downloader".equals( message.body() ) ){
 				EventBus eb = vertx.eventBus();
-				eb.publish("shop.download.parse", "http://192.168.0.53/");
+				eb.publish("shop.download.parse", "http://192.168.0.1/");
 			}
-		}
+		};
 
-	};
+		Handler<Message> shop_load_complete = new Handler<Message>() {
+			public void handle( Message message ){
+				System.out.println("I received a message " + message.body() );
 
-	eb.registerHandler("shop.parse.parse", myHandler );
-	eb.registerHandler("shop.load.complete", shop_load_complete );
+				// initialized downloader ( test mode )
+				if( "downloader".equals( message.body() ) ){
+					EventBus eb = vertx.eventBus();
+					eb.publish("shop.download.parse", "http://192.168.0.1/");
+				}
+			}
+		};
 
-	/////////////////////////////////////////////////////////////////
-	// modules Run
-	// Run downloader
-	container.deployModule( "io.vertx~mod-shopcrawler-downloader~0.0.1-alpha1", config );
+		eb.registerHandler("shop.parse.parse", myHandler );
+		eb.registerHandler("shop.load.complete", shop_load_complete );*/
 
-	
-	
-  }
+
+		/////////////////////////////////////////////////////////////////
+		// modules Run
+		// Run downloader
+		container.deployModule ("io.vertx~mod-shopcrawler-test~0.0.1-alpha1", config);
+		//container.deployModule( "io.vertx~mod-shopcrawler-downloader~0.0.1-alpha1", config );
+		container.deployModule ("io.vertx~mod-shopcrawler-informSaver~0.0.1-alpha1", config);
+	}
 }
