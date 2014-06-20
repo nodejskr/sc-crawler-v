7a7b54a40e329f5c9ffa6933bdb3a33a475c2b2c
서병선 <famersbs@seobyeongseon-ui-MacBook-Air.local>
2014-06-20 19:50:54 +0900

    listamanager update

--- mods/io.vertx~mod-shopcrawler-listmanager~0.0.1-alpha1/listmanager.java
+++ mods/io.vertx~mod-shopcrawler-listmanager~0.0.1-alpha1/listmanager.java
@@ -10,28 +10,51 @@ import org.vertx.java.core.http.HttpClientResponse;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
 
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.json.JsonArray;
+
 public class listmanager extends Verticle {
 
   static EventBus eb;
 
-  /*******************************************
-   * shop add msg handler
-   ******************************************/
-  private class shop_list_add implements Handler<Message>
-  {
-	public void handle( Message message ){
-		//System.out.println("ListManager : --> Download" + message.body() );
-		// 지금은 그냥 바로 downloader 로 넘길까?
-		eb.send( "shop.download.parse", message.body() );
-	}
-  };
+  private static void refreshSiteList(){
+
+  		// Request Data --> after write test code then move to timer
+	eb.send( "shop.site.list", "" , new Handler<Message<JsonArray>>(){
+		public void handle( Message<JsonArray> message ){
+
+			//JsonObject obj = message
+			JsonArray list = message.body();
+
+			for( int i = 0 ; i < list.size() ; ++ i ){
+
+				JsonObject e = list.get( i );
+
+				System.out.println("recv site_list " + i + " " + e.getString("url") );
+
+				eb.send( "shop.download.parse", e.getString("url")  );
+
+			}
+
+			
+
+		}
+	} );
+
+  }
 
   public void start() {
     
 	System.out.println("Start List Manager");
 	
 	eb = vertx.eventBus();
-	eb.registerHandler("shop.list.add", new shop_list_add() );
+
+	// wait start signal
+	eb.registerHandler("shop.start", new Handler<Message>(){
+		public void handle( Message message ){
+			refreshSiteList();
+		}
+	});
 
 	// publish cloadComplete msg 
 	eb.publish( "shop.module.load.complete", "listmanager");
