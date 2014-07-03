7d2b03f6c4da00e41c7da279463cc9e660489d41
floatics <floatics@floaticsui-iMac.local>
2014-07-03 19:22:25 +0900

    parser 구조 변경

---
+++ mod-shopcrawler-infomanager/src/main/java/io/vertx/shopcrawler/parser/ParserCore.java
@@ -0,0 +1,7 @@
+package io.vertx.shopcrawler.parser;
+
+import org.vertx.java.core.json.JsonArray;
+
+public abstract class ParserCore {
+	public abstract JsonArray parse();
+}
---
+++ mod-shopcrawler-infomanager/src/main/java/io/vertx/shopcrawler/parser/ParserMain.java
@@ -0,0 +1,39 @@
+package io.vertx.shopcrawler.parser;
+
+import org.vertx.java.busmods.BusModBase;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+
+public class ParserMain extends BusModBase implements
+		Handler<Message<JsonObject>> {
+	private String baseAddress = null;
+
+	public void start() {
+		super.start();
+		JsonObject config = container.config();
+		baseAddress = config.getString("address", "shop.parse.parse");
+		eb.registerHandler(baseAddress, this);
+	}
+
+	@Override
+	public void handle(Message<JsonObject> message) {
+		String[] data = message.body().toString().split("\n");
+		String url = data[0], type = data[1], body = data[2];
+		ParserCore parser = null;
+		switch (type) {
+		case "cafe24":
+			parser = new ParserCafe24DBurl(url, "euc-kr");
+			break;
+		default:
+			break;
+		}
+		JsonObject jsonData = new JsonObject();
+		jsonData.putString("command", "addProductInfo");
+		jsonData.putArray("data", parser.parse());
+		eb.publish("shop.saver.product.save", jsonData);
+		sendOK(message);
+		message.reply();
+	}
+
+}
