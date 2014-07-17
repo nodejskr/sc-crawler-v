c12d9dd66ce5af645afea141f1ba560450c9ca3b
서병선 <famersbs@gmail.com>
2014-07-17 21:21:56 +0900

    한글 인코딩 깨지는거 수정

--- mods/io.vertx~mod-shopcrawler-downloader~0.0.1-alpha1/downloader.java
+++ mods/io.vertx~mod-shopcrawler-downloader~0.0.1-alpha1/downloader.java
@@ -58,6 +58,9 @@ public class downloader extends Verticle {
 	}
 	  	
 	public void handle(Buffer data) {
+
+		System.out.println( data.getString( 0, data.length(), "euc-kr" ) );
+
 		downloader.eb.send("info.call.parser", url + "\n" + type + "\n" + data.toString() );
 	}
   };
